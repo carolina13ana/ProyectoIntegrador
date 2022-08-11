278bca5d90dc94c9b591414746d1ef7a5aa3062f
FloriMartin <99359265+FloriMartin@users.noreply.github.com>
2022-08-11 03:21:20 -0300

    Cambio nombre lista
    Se tenía que llamar Perro2

--- Ejercicio_4_Pyton_Martin_Florencia.py
+++ Ejercicio_4_Pyton_Martin_Florencia.py
@@ -1,3 +1,3 @@
-perro = [14, "Fido", "12/02/2012", "Macho", 23546987]
-for i in range(len(perro)-1, -1, -1):
-    print (perro[i])
+perro2 = [14, "Fido", "12/02/2012", "Macho", 23546987]
+for i in range(len(perro2)-1, -1, -1):
+    print (perro2[i])
