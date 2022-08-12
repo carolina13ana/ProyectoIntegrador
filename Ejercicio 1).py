f7cba330c5dee17f38aa158209caf7f7ac5c8930
Carolina Heredia <107896197+carolina13ana@users.noreply.github.com>
2022-08-12 01:35:50 -0300

    Ejercicio de Python

---
+++ Ejercicio 1).py
@@ -0,0 +1,9 @@
+﻿Dueno=[28957346,'Juan','Perez',4789689,'Belgrano 101']
+
+dni=int(input("¿Cuál es tu DNI?: "))
+while dni < 26E6:
+  print("lo siento, vuelve a intentarlo")
+  dni=int(input("¿Cuál es tu DNI?: "))
+  
+print("Su número de teléfono es:", Dueno[3])
+
